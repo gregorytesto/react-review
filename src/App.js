7769323a855ebb227a2c8c1dd33cbad655dcd15f
Greg Testo <gregt@pursuit.org>
2021-11-09 14:29:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+// function MyComp(){
+//   return <div>Turkey, turkey</div>
+// }
+
+// //                              App
+// //            ChildComp                     ChildComp
+// //          GrandChildComp                GrandChildComp
+// //         GreatGrandChildComp          GreatGrandChildComp
+
+// function ChildComp(){
+//   return (
+//     <div>
+//       <div>Child Component</div>
+//       <GrandChildComp />
+//     </div>
+//   )
+// }
+
+// function GrandChildComp(){
+//   return (
+//     <div>
+//         <div>Grand Child Component</div>
+//         <GreatGrandChildComp />
+//     </div>
+//   )
+// }
+
+// function GreatGrandChildComp(){
+//   return <div>Great Grand Child Component</div>
+// }
+
+// function App(){
+//   return (
+//     <div>
+//       <div>Hello World</div>
+//       <ChildComp />
+//       <ChildComp />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//             App
+//             List
+// ListItem ListItem ListItem
+
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>App</div>
+      <List />
     </div>
-  );
+  )
+}
+
+function List(){
+  return (
+    <div>
+      <div>List</div>
+      <ListItem title="Turkey" numOfItems={1} />
+      <ListItem title="Cranberries" numOfItems={5} />
+      <ListItem title="Gravy" numOfItems={1} />
+    </div>
+  )
+}
+
+// function ListItem(props){
+//   return <div>List Item: {props.numOfItems} {props.title}</div>
+// }
+
+class ListItem extends Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return <div>List Item: {this.props.numOfItems} {this.props.title}</div>
+  }
 }
 
 export default App;
